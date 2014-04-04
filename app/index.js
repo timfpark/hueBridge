8205f554c1e7475c6877f16af1839fbee4449c6d
Tim Park <timfpark@gmail.com>
2014-04-04 13:10:40 -0700

    update hue-bridge to object application model

--- app/index.js
+++ app/index.js
@@ -3,57 +3,66 @@ var hue = require("node-hue-api")
   , nitrogen = require('nitrogen')
   , PhilipsHueLight = require('nitrogen-philips-hue');
 
-var session;
-var params;
+function HueBridge(session, params) {
+    this.session = session;
+    this.params = params;
+}
 
-var start = function(s, p) {
-    session = s;
-    params = p;
+HueBridge.prototype.connectLights = function() {
+    var self = this;
 
-    hue.locateBridges(function(err, bridges) {
-        if (err) return session.log.error('error while locating bridges: ' + err);
+    this.findLights(function(err, lights, hueApi, bridge) {
+        if (err) return self.session.log.error(err);
 
-        bridges.forEach(function(bridge) {
-            session.log.info('bridge: ' + JSON.stringify(bridge));
-            var hueApi = new hue.HueApi(bridge.ipaddress, "5bdeb8c11a332cf33cc9f64b7b1267");
+        lights.forEach(function(lightObj) {
+            self.session.log.info('light: ' + JSON.stringify(lightObj));
 
-            hueApi.lights(function(err, result) {
-                if (err) return session.log.error('error while locating lights: ' + err);
+            var hueLight = new PhilipsHueLight({
+                hue_api: hueApi,
+                name: lightObj.name,
+                nickname: bridge.id + "_" + lightObj.id,
+                philips_id: lightObj.id
+            });
 
-                session.log.info('lights: ' + JSON.stringify(result.lights));
+            self.session.log.info('connecting light: ' + hueLight.name);
+            self.session.service.connect(hueLight, function(err, lightSession, hueLight) {
+                if (err) return self.session.log.error('error while connecting light: ' + err);
 
-                result.lights.forEach(function(lightObj) {
-
-                    session.log.info('light: ' + JSON.stringify(lightObj));
-
-                    var hueLight = new PhilipsHueLight({
-                        hue_api: hueApi,
-                        name: lightObj.name,
-                        nickname: bridge.id + "_" + lightObj.id,
-                        philips_id: lightObj.id
-                    });
-
-                    session.log.info('connecting light: ' + hueLight.name);
-                    session.service.connect(hueLight, function(err, session, hueLight) {
-                        if (err) return session.log.error('error while connecting light: ' + err);
-
-                        session.log.info('starting light manager for light: ' + hueLight.name);
-                        new LightManager(hueLight).start(session, function(err, message) {
-                            if (err) return session.log.error(JSON.stringify(err));
-                        });
-                    });
+                lightSession.log.info('starting light manager for light: ' + hueLight.name);
+                new LightManager(hueLight).start(lightSession, function(err, message) {
+                    if (err) return lightSession.log.error(JSON.stringify(err));
                 });
             });
         });
     });
-
 };
 
-var stop = function() {
+HueBridge.prototype.findLights = function(callback) {
+    var self = this;
 
+    hue.locateBridges(function(err, bridges) {
+        if (err) return callback('error while locating bridges: ' + err);
+
+        bridges.forEach(function(bridge) {
+            self.session.log.info('bridge: ' + JSON.stringify(bridge));
+            var hueApi = new hue.HueApi(bridge.ipaddress, "5bdeb8c11a332cf33cc9f64b7b1267");
+
+            hueApi.lights(function(err, result) {
+                if (err) return self.session.log.error('error while locating lights: ' + err);
+
+                self.session.log.info('lights: ' + JSON.stringify(result.lights));
+                return callback(null, result.lights, hueApi, bridge);
+            });
+        });
+    });
 };
 
-module.exports = {
-    start: start,
-    stop: stop
-}
+HueBridge.prototype.start = function() {
+    this.bridge = new HueBridge(this.session, this.params);
+    this.bridge.connectLights();
+};
+
+HueBridge.prototype.stop = function() {
+};
+
+module.exports = HueBridge;
